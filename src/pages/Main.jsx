79e7313a44d788a86ccdfe60b61ed65d479a5e74
kbs0711 <byoungsu0711@gmail.com>
2023-08-17 14:03:57 +0900

    login experiment

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -21,7 +21,7 @@ export default function Main() {
   return (
     <>
 
-      {logged ? <div><p>로그인 상태 {email}</p> <button onClick={()=>signOut()}>로그아웃</button></div> : <div><p>로그아웃 상태</p><button onClick={()=>router.push("/")}>로그인</button></div>}
+      {!logged ? <div><p>로그인 상태 {email}</p> <button onClick={()=>signOut()}>로그아웃</button></div> : <div><p>로그아웃 상태</p><button onClick={()=>router.push("/")}>로그인</button></div>}
       {loggedinByEmail && <p>{loginEmail}</p>}
       <Fetch />
     </>
